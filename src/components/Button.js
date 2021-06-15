77b475d1fc0e1f6fd0dc49f04133ebda7fd1e287
Vitinho <vitinho.dev@gmail.com>
2021-06-15 12:14:50 -0300

    estudo rocketseat

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Text, StyleSheet, TouchableOpacity } from "react-native"
+
+
+function Button({ onPress }) {
+  return (
+    <TouchableOpacity
+        style={styles.button}
+        activeOpacity={.7}
+        onPress={onPress}
+    >
+      <Text style={styles.buttonText}>Add</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#A370F7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: "bold",
+  }
+})
+
+export { Button }
---
+++ src/components/SkillCard.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { TouchableOpacity, Text, StyleSheet } from "react-native"
+
+function SkillCard({ skill }) {
+  return (
+    <TouchableOpacity style={styles.buttonSkill}>
+      <Text style={styles.textSkill}>
+        {skill}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: "#1F1E25",
+    padding: 20,
+    borderRadius: 20,
+    alignItems: "center",
+    marginVertical: 10
+  },
+  textSkill: {
+    color: "#fff",
+    fontSize: 22,
+    fontWeight: "bold",
+  }
+})
+
+export { SkillCard }
